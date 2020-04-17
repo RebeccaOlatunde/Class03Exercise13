d4aa6c5784efce6735d7696f988b7d0cdccc8425
RebeccaOlatunde <54245689+RebeccaOlatunde@users.noreply.github.com>
2020-04-16 23:46:02 -0400

    Update Program.cs

--- Class03Exercise13/Program.cs
+++ Class03Exercise13/Program.cs
@@ -7,6 +7,31 @@ namespace Class03Exercise13
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            tring response = "";
+
+            // For int i = 0; i <= 5; i++
+            // Console.ReadLine("Enter a number less than 5)");
+            // Console.ReadLine("Enter a number less than 12");
+
+            do
+            {
+                //For loop
+
+                Console.WriteLine("Enter a number");
+                var num = Console.ReadLine();
+                var number = int.Parse(num);
+                for (int i = 0; i < number; i++)
+				{
+                    Console.WriteLine(i);
+				}
+
+                Console.WriteLine("Would you like to continue?y/n");
+                response = Console.ReadLine();
+            }   while (response == "y");
+
+            Console.WriteLine("Goodbye!");
+
+
         }
     }
 }
